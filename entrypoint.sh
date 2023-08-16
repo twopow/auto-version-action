38c5da7f05380565c3ce37f18b142aeb4ac4cb8f
Patrick McCarren <patrick@cumulative.io>
2023-08-15 23:02:48 -0400

    fix: multiline output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@
 set -euf
 
 echo "version=$(git describe --tags --always 2>/dev/null || echo -n 'unknown')" >>"$GITHUB_OUTPUT"
-echo "shortlog=$(git shortlog "$(git describe --tags --always --abbrev=0 2>/dev/null)"..HEAD -e || echo -n 'unknown')" >>"$GITHUB_OUTPUT"
+
+{
+  echo "shortlog<<GHEOF"
+  git shortlog "$(git describe --tags --always --abbrev=0 2>/dev/null)"..HEAD -e || echo -n 'unknown'
+  echo "GHEOF"
+} >>"$GITHUB_OUTPUT"
