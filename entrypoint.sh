8dfed9cc4cbd08fd87f1cb521a464f4fcc918ab7
Patrick McCarren <patrick@cumulative.io>
2026-01-06 13:44:07 -0600

    feat: short commit sha extraction

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,14 @@ GIT_TAG=$(git describe --tags --always 2>/dev/null);
 GIT_TAG=${GIT_TAG:-'unknown'};
 echo "tag=${GIT_TAG}" >>"$GITHUB_OUTPUT";
 
+GIT_SHORTSHA=$(git rev-parse --short HEAD 2>/dev/null);
+GIT_SHORTSHA=${GIT_SHORTSHA:-'unknown'};
+echo "shortsha=${GIT_SHORTSHA}" >>"$GITHUB_OUTPUT";
+
 # if VERSION is not set, use the git tag
 VERSION=${VERSION:-${GIT_TAG}};
+VERSION=$(echo -n "${VERSION}" | sed 's/[^0-9a-zA-Z_\-\.]/-/g');
+
 echo "version=${VERSION}" >>"$GITHUB_OUTPUT"
 echo -n "${VERSION}" > version.txt;
 
